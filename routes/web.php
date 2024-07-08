57e8ee3b9882f81a63011bf3c6c93f7857c574b1
rizkydimedan <rizkyanakmedan77@gmail.com>
2024-07-08 16:31:07 +0700

    master

--- routes/web.php
+++ routes/web.php
@@ -40,10 +40,6 @@ Route::middleware(['auth', 'verified', 'admin'])->group(function(){
 });
 
 
-
-
-// Route::get('/dashboard',[DashboardController::class, 'index'])->middleware(['auth','verified', 'admin'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
